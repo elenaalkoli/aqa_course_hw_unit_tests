a032598c832cf0108d673c1aaaacc535338c2170
elenaalkoli <alkolielena@gmail.com>
2025-07-31 17:10:22 +0300

    added task-1, task-2, task-3 for hw-4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,19 @@
   преобразовываясь в number
 
 */
+
+  const minAge = 18;
+  const maxAge = 60;
+  let age = '100';
+  if (isNaN(age)){
+    console.log('Incorrect data type')
+  }
+  else if (age < minAge){ //при сравнении итак идет приведение типов - в числа, а потом в boolean
+    console.log(`You don't have access cause your age is ${age}, it's less then ${minAge}`)
+  }
+  else if (age >= minAge && age < maxAge){
+    console.log('Welcome!')
+  }
+  else if (age > maxAge){
+    console.log('Keep calm and look Culture channel!')
+  } else console.log('Technical work');
