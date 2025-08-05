71333bf2f736d96e421b48f29d3b8a3d9acdd10b
elenaalkoli <alkolielena@gmail.com>
2025-08-05 14:27:18 +0300

    added hw-5 (task-1, task-2, task-3)

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -5,6 +5,26 @@
  */
 const word = 'hello';
 
-let vowelsAndConsonantsResult = '';
+let vowelsCount = 0;
+let consonantsCount = 0; //счетчики букв
 
-export { vowelsAndConsonantsResult };
+const vowels = 'aeiou';
+const consonants = 'bcdfghjklmnpqrstvwxyz';
+
+let vowelsAndConsonantsResult = '';
+if (!word || typeof word !== "string" || !/[a-zA-Z]/.test(word)) { // RegEx - содержит ли строка хотя бы одну англ.букву (если в строке нет ни одной буквы — false)
+    console.log('Please provide a valid string with letters')
+} else {
+for (let i = 0; i < word.length; i++){ // word.length всегда +1 индекс, то есть последний индекс length-1, а значит i < word.length 
+    let char = word[i].toLowerCase();
+    if(vowels.includes(char)){
+        vowelsCount++;
+    } else if (consonants.includes(char)) {
+        consonantsCount++;
+    }
+}
+vowelsAndConsonantsResult = `${word} contains ${vowelsCount} vowels and ${consonantsCount} consonants`;
+console.log(vowelsAndConsonantsResult)
+}
+
+export { vowelsAndConsonantsResult }; 
