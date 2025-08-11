1ebf2aafc5fa6b68cde36401a392104e34dd1559
elenaalkoli <alkolielena@gmail.com>
2025-08-11 13:53:39 +0300

    added hw-6 (task-1, task-2, task-3)

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,39 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+let resultUnique = [];
 let resultNull;
+
+//competitorPizzas к нижнему регистру 
+const competitorPizzasInLowerCase = [];
+for (const item of competitorPizzas){
+  competitorPizzasInLowerCase.push(item.toLowerCase())
+}
+
+// сравниваю myPizzasT1 в нижнем регистре с competitorPizzasInLowerCase
+for (const item of myPizzasT1) {
+  if (!competitorPizzasInLowerCase.includes(item.toLowerCase())){
+    resultUnique.push(item); // push возвращает новую длину массива! поэтому нельзя тут присваивать сразу в resultUnique
+  }
+}
+
+// сравниваю myPizzasT2 в нижнем регистре с competitorPizzasInLowerCase
+let isUniqueT2 = false;
+for (const item of myPizzasT2) {
+  if (!competitorPizzasInLowerCase.includes(item.toLowerCase())){
+    isUniqueT2 = true;
+    break; //хотя бы одно совпадение - выходим из цикла
+    }
+}
+
+if (!isUniqueT2) { 
+  resultNull = null;
+}
+
+console.log(resultUnique, resultNull);
+
 
 export { resultNull, resultUnique };
--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -6,6 +6,17 @@
 
   Присвойте результат в переменную "unique"
 */
-let unique;
+let unique = [];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+
+for (const item of arr){
+  if (!unique.includes(item)) {
+    unique.push(item);
+  }
+}
+console.log(unique)
+
+//или
+// const unique = [...new Set(arr)];
 
 export { unique };
