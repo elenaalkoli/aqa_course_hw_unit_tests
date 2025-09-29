66d59ae48a4c928f23f7af6397c022c402574f16
elenaalkoli <alkolielena@gmail.com>
2025-09-29 14:16:01 +0300

    added extra solutions: getKeyByValue implemented with Object.keys and Object.entries

--- homework-solutions/js/hw-15/task-2.ts
+++ homework-solutions/js/hw-15/task-2.ts
@@ -19,3 +19,13 @@ const roles = {
 };
 
 console.log(getKeyByValue(roles, "GUEST"));
+
+//ИЛИ через Object.keys(obj)
+function getKeyByValue2<T extends object>(obj: T, value: T[keyof T]): keyof T | undefined {
+    return (Object.keys(obj) as (keyof T)[]).find(key => obj[key] === value);
+}
+//ИЛИ через Object.entries(obj)
+function getKeyByValue3<T extends object>(obj: T, value: T[keyof T]): keyof T | undefined {
+    const entry = Object.entries(obj).find(([key,val])=> val === value);
+    return entry ? entry[0] as keyof T : undefined;
+}
